d1ef5053ee351900e7eee9fba366c3cf442ba430
Linda <linda@hellomynameislinda.com>
2024-04-16 15:20:33 +0200

    Some random test stuf.

---
+++ Grupp7-GitOvning/Models/LindasTestClass.cs
@@ -0,0 +1,14 @@
+﻿namespace Grupp7_GitOvning.Models
+{
+    public class LindasTestClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string APointlessMethod()
+        {
+            return $"{Id}: {Name}\n{Description}";
+        }
+    }
+}
